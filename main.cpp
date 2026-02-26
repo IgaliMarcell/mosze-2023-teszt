02a3e6cc98332d846612c559fa3f6acba1c3a40d
Igali Marcell <i.marci158@gmail.com>
2026-02-26 08:02:14 +0100

    Hibák javítva

--- main.cpp
+++ main.cpp
@@ -4,24 +4,27 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //NELEMENTS nem létezik -> N_ELEMENTS viszont igen
-    std::cout << '1-100 ertekek duplazasa' // Rossz aposztróf használata és nincsen lezárva a sor ;-vel
-    for (int i = 0;) // Befejezetlen for ciklus -> hiányzik a feltétel és az iteráció
+    int *b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        b[i] = i * 2; // Értékadásnál (i+1) kell, hogy ne a 0-99, hanem az 1-100 intervallumot kapjuk
+        b[i] = (i+1) * 2;
     }
-    for (int i = 0; i; i++) // Befejezetlen for ciklus -> hiányos a megállási feltétel
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:" // A sor nincsen lezárva ;-vel. Hiányzik az érték kiírása
+        std::cout << "Ertek: " << b[i] << std::endl;
     }
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; // Nincsen kezdőérték -> nem lehet növelni az értékét (memória szemét)
-    for (int i = 0; i < N_ELEMENTS, i++) // , használata ; helyett
+    int atlag = 0;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        atlag += b[i] // A sor nincsen lezárva ;-vel
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
-    // A mutató nincsen törölve a program kilépése előtt -> MEMÓRIASZIVÁRGÁS
+    
+    delete b;
+    b = 0;
+    
     return 0;
 }
